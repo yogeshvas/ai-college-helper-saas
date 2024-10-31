b3bdb4b42e262ed3798a2ba257ad701ca5d6c8de
Yogesh Vashisth <writetokhair@gmail.com>
2024-11-01 01:01:22 +0530

    rought changes

--- components/custom/stats.tsx
+++ components/custom/stats.tsx
@@ -4,11 +4,12 @@ export function DownloadStats() {
   return (
     <>
       <div className="flex gap-2 items-center">
-        <HyperText text="400+" />
-        <div>Downloads</div>
-        <div className="rounded-full bg-slate-900">|</div>
-        <div className="">
-          Powered by <span className="text-blue-500 ">Gemini AI</span>
+        {/* <HyperText text="400+" />
+        <div>Downloads</div> */}
+        {/* <div className="rounded-full bg-slate-900">|</div> */}
+        <div className="flex items-center gap-2 justify-center">
+          Powered by
+          <span className="text-blue-500 text-lg">Gemini AI</span>
         </div>
       </div>
     </>
